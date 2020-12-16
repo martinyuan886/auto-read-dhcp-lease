984c79f109bcb8cdf57a7b80fbdbc11511acfd9c
martinyuan886 <nulizhu_86@hotmail.com>
2020-12-16 19:53:56 +0800

    add note

--- get_ip_mac_in_lease.py
+++ get_ip_mac_in_lease.py
@@ -1,3 +1,20 @@
+#############
+### usage ###
+#############
+#use default dhcp lease file(/var/lib/dhcpd/dhcpd.leases)
+# python get_ip_mac_in_lease.py
+
+#specify target dhcp lease file
+#python get_ip_mac_in_lease.py /path/to/dhcpd.leases
+
+############
+### NOTE ###
+############
+#dev under python 3.8.6 on centos7
+
+#install requirements by using:
+# pip instsall -r requrirement.txt
+
 import sys
 from  iscdhcpleases import Lease, IscDhcpLeases
 
